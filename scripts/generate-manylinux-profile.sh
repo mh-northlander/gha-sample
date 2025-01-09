2851a66d7599b7b3c9bf0dc5742e04b60ab36373
hayashi_mas <hayashi_mas@worksap.co.jp>
2025-01-09 18:23:30 +0900

    fix pwd, install rustup

--- scripts/generate-manylinux-profile.sh
+++ scripts/generate-manylinux-profile.sh
@@ -3,11 +3,6 @@ set -ex
 
 # This script is assumed to be used inside https://github.com/pypa/manylinux.
 
-DIR=$(dirname "$(readlink -f "$0")")
-cd "$DIR/.."
-
-cd sudachi.rs/
-
 TARGET_TRIPLE=$(rustc -vV | awk '/^host/ {print $2}')
 PROFDATA=/tmp/sudachi-profdata
 
@@ -30,21 +25,3 @@ target/release/sudachi --all --mode=B -o /dev/null leads.txt
 # Generate Merged PGO data
 "$HOME/.rustup/toolchains/stable-$TARGET_TRIPLE/lib/rustlib/$TARGET_TRIPLE/bin/llvm-profdata" \
   merge -o /tmp/sudachi-profdata.merged "$PROFDATA"
-
-# cd python/
-
-# export RUSTFLAGS='-C profile-use=/tmp/sudachi-profdata.merged -C opt-level=3'
-# export CARGO_BUILD_TARGET=$TARGET_TRIPLE
-
-# # see following link for the list of cpython bin
-# # https://github.com/pypa/manylinux?tab=readme-ov-file#image-content
-# for PYBIN in /opt/python/cp{39,313}-*/bin; do
-#     "${PYBIN}/pip" install -U setuptools wheel setuptools-rust
-#     find . -iname 'sudachipy*.so'
-#     rm -f build/lib/sudachipy/sudachipy*.so
-#     "${PYBIN}/pip" wheel . --no-build-isolation -vvv --wheel-dir ./dist
-# done
-
-# for whl in dist/*.whl; do
-#     auditwheel repair "$whl" -w dist/
-# done
